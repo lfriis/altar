a7d1903e0aebd17a18220a2f2a2829019ad917bc
Larsen Friis <larsenfriis@icloud.com>
2022-05-01 00:44:55 -0400

    fix: update server start script

--- server/dist/src/server.js
+++ server/dist/src/server.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -8,6 +17,7 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
+const path_1 = __importDefault(require("path"));
 const routes_1 = __importDefault(require("./routes"));
 const config_1 = require("./config");
 const middleware_1 = require("./middleware");
@@ -41,6 +51,16 @@ server
         message: 'Error deploying server',
         object: err,
     });
+});
+/**
+ * ? Serving static files
+ */
+server.get('/*', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, '../client/build/index.html'), (e) => __awaiter(void 0, void 0, void 0, function* () {
+        if (e) {
+            res.status(500).send(e);
+        }
+    }));
 });
 /**
  * ? Error handling to help with unhandled rejection
